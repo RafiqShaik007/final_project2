b7ce5cc25c70b5e2179cf96b28d9057499b37390
Rafiq Shaik <141270246+RafiqShaik007@users.noreply.github.com>
2024-11-22 16:26:43 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,28 @@
+
+
+function reverse_string(str){
+console.log(  str.split('').reverse().join('')  )
+}
+
+
+
+
+// reverse_string('Abhishek')
+// reverse_string('Pawan')
+// reverse_string('Ganesh')
+
+
+// function birthday_wishes(name){
+    
+//     if(date === today.date){
+//         console.log('Happiiiiiiiiiiiiesssssstttttttt Birthday Heeerrroooo ', name)
+//     }
+//     else{
+//         console.log('Happy Birthday ', name)
+//     }
+// }
+
+
+// birthday_wishes('Pawan')
+// birthday_wishes('Abhishek')
+// birthday_wishes('Ganesh', '01-02')
